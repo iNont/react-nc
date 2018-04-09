0245ecd04c72e08f01bac3046426e57098140145
Natchanon Charoensuk <inont@hotmail.co.th>
2018-04-09 10:54:23 +0700

    Version 1.0.27

--- src/RouteControl.js
+++ src/RouteControl.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Redirect } from 'react-router';
+import { Redirect } from 'react-router-dom';
 import CTRL from './CTRL';
 
 class RouteControlComponent extends React.Component {
