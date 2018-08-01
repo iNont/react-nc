eb3c14ad31d4db0b9e14aa53f478777cefe6037f
Natchanon Charoensuk <inont@hotmail.co.th>
2018-08-01 16:03:41 +0700

    Storage using object

--- src/CTRL.js
+++ src/CTRL.js
@@ -4,6 +4,7 @@ const CTRL = {
   app: undefined,
   debug: false,
   reactNCLog: false,
+  state: {},
   initialState: {},
   routing: {},
   log: (text) => { if(CTRL.debug) console.log(`React-nc: ${text}`); },
@@ -17,29 +18,22 @@ const CTRL = {
     if(!CTRL.app) {
       throw new Error("Application wrapped by StateControl is not found");
     }
-    CTRL.app.state = {
-      ...CTRL.app.state,
+    CTRL.state = {
+      ...CTRL.state,
       ...state
     };
     if(typeof callback === 'function') {
       callback();
     }
-    CTRL.app.forceUpdate();
+    CTRL.forceUpdate();
   },
   resetState: () => {
-    CTRL.setState(Utils.deepCopy(CTRL.initialState));
+    CTRL.state = Utils.deepCopy(CTRL.initialState);
+    CTRL.forceUpdate();
    },
   initializeState: state => {
     CTRL.initialState = state;
-    if(CTRL.app) {
-      CTRL.resetState();
-    }
-  },
-  get state() {
-    if(!CTRL.app) {
-      throw new Error("Application wrapped by StateControl is not found");
-    }
-    return CTRL.app.state;
+    CTRL.state = Utils.deepCopy(CTRL.initialState);
   }
 };
 
